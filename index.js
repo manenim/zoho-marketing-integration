7b9ad94323dafd00490d2285aa0b80b92c9b302e
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2023-04-24 11:58:55 +0100

    added secrets

--- index.js
+++ index.js
@@ -20,66 +20,53 @@ app.use(express.json());
 app.use(cors())
 
 
-// app.get("/users/:userId", async function (req, res) {
-//   const params = {
-//     TableName: USERS_TABLE,
-//     Key: {
-//       userId: req.params.userId,
-//     },
-//   };
+app.get("/users/:userId", async function (req, res) {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
+  };
 
-//   try {
-//     const { Item } = await dynamoDbClient.send(new GetCommand(params));
-//     if (Item) {
-//       const { userId, name } = Item;
-//       res.json({ userId, name });
-//     } else {
-//       res
-//         .status(404)
-//         .json({ error: 'Could not find user with provided "userId"' });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ error: "Could not retreive user" });
-//   }
-// });
+  try {
+    const { Item } = await dynamoDbClient.send(new GetCommand(params));
+    if (Item) {
+      const { userId, name } = Item;
+      res.json({ userId, name });
+    } else {
+      res
+        .status(404)
+        .json({ error: 'Could not find user with provided "userId"' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not retreive user" });
+  }
+});
 
-// app.post("/users", async function (req, res) {
-//   const { userId, name } = req.body;
-//   if (typeof userId !== "string") {
-//     res.status(400).json({ error: '"userId" must be a string' });
-//   } else if (typeof name !== "string") {
-//     res.status(400).json({ error: '"name" must be a string' });
-//   }
+app.post("/users", async function (req, res) {
+  const { userId, name } = req.body;
+  if (typeof userId !== "string") {
+    res.status(400).json({ error: '"userId" must be a string' });
+  } else if (typeof name !== "string") {
+    res.status(400).json({ error: '"name" must be a string' });
+  }
 
-//   const params = {
-//     TableName: USERS_TABLE,
-//     Item: {
-//       userId: userId,
-//       name: name,
-//     },
-//   };
+  const params = {
+    TableName: USERS_TABLE,
+    Item: {
+      userId: userId,
+      name: name,
+    },
+  };
 
-//   try {
-//     await dynamoDbClient.send(new PutCommand(params));
-//     res.json({ userId, name });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ error: "Could not create user" });
-//   }
-// });
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.use('/waitlist', waitlistRoute)
-
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
+  try {
+    await dynamoDbClient.send(new PutCommand(params));
+    res.json({ userId, name });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Could not create user" });
+  }
 });
 
 
