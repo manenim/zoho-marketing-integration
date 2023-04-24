a1ffff9be6ca6d630c40a072332d9c9622fc5103
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2023-04-24 13:46:14 +0100

    added secrets

--- routes/waitlist.js
+++ routes/waitlist.js
@@ -1,11 +1,15 @@
 
 const express = require('express');
-const addToWaitlist = require('../controllers/waitlist/addToWaitlist');
+// const addToWaitlist = require('../controllers/waitlist/addToWaitlist');
 
 const router = express.Router();
 
 
 
-router.post('/add', addToWaitlist);
+// router.post('/add', addToWaitlist);
+router.post('/add', (req, res) => {
+    email = req.body.email
+    res.send(`${email} added to waitlist!`)
+});
 
 module.exports = router;
