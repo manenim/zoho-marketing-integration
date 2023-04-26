bd974e3513a73ce638f2573b647dc41b8e7ec3b0
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2023-04-26 13:29:23 +0100

    added secrets

--- controllers/waitlist.js
+++ controllers/waitlist.js
@@ -7,17 +7,16 @@ const accessToken = process.env.ACCESS_TOKEN
 const refreshToken = process.env.REFRESH_TOKEN
 const addToWaitlist = async (req, res) => {
     let { email } = req.body
-    // let hashedEmail = req.headers['email-hash']
+    let hashedEmail = req.headers['email-hash']
 
-    // const match = await bcrypt.compare(email, hashedEmail);
 
     res.json({email})
 
-    // if (!validator.isEmail(email)) {
-    //     return res.status(400).send('Invalid email address');
-    // }
-    // const match = await bcrypt.compare(email, hashedEmail);
-    // console.log(match)
+    if (!validator.isEmail(email)) {
+        return res.status(400).send('Invalid email address');
+    }
+    const match = await bcrypt.compare(email, hashedEmail);
+   res.json({email, hashedEmail, match, listKey, accessToken, refreshToken})
 
     // res.send('Success')
 }
