19c6105c1e8147d69009c4f8e53751771b44f5e5
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2023-04-26 14:40:13 +0100

    added secrets

--- controllers/waitlist.js
+++ controllers/waitlist.js
@@ -2,19 +2,18 @@ const bcrypt = require('bcrypt')
 const validator = require('validator')
 const axios = require('axios')
 
-
+// DECLARING VARIABLES 
 let accessToken = null
 const refreshToken = process.env.REFRESH_TOKEN
 const clientId = process.env.CLIENT_ID
 const clientSecret = process.env.CLIENT_SECRET
+const zohoUrl = process.env.ZOHO_AUTH_URL
 
-
+// get access token
 const refreshAccessToken = async () => {
-
-   const data = await axios.post(`https://accounts.zoho.com/oauth/v2/token?client_id=${clientId}&grant_type=refresh_token&client_secret=${clientSecret}&refresh_token=${refreshToken}`)
+   const data = await axios.post(`${zohoUrl}?client_id=${clientId}&grant_type=refresh_token&client_secret=${clientSecret}&refresh_token=${refreshToken}`)
     accessToken = data.data.access_token
     expirationTime =  Date.now() + (data.data.expires_in * 1000);
-
 }
 
 
@@ -29,22 +28,30 @@ const tokenMiddleware = async (req, res, next) => {
 
 
 
-
+// add to waitlist (lambda function)
 const addToWaitlist = async (req, res) => {
+
     let { email } = req.body
     let hashedEmail = req.headers['email-hash']
-    // validate email
+
+
+    // validate email address
     if (!validator.isEmail(email)) {
         return res.status(400).send('Invalid email address');
     }
 
-    // validate hashed email
+    // decrypt and compare hashed email to email address in request body
     const match = await bcrypt.compare(email, hashedEmail);
 
-    const url = 'https://marketingautomation.zoho.com/api/v1/json/listsubscribe';
-    const listKey = process.env.LIST_KEY
+    
+    // if hashed email is not valid, return error
+    if (!match) {
+        return res.status(403).send('You are not authorized to access this resource');
+    }
 
-    // const listkey = '3zb6c2ec6e5eb01250c4d8907fdaeb777cb51dfa03959765cb3b8b9737a8a4785e';
+    // if email is valid, add to waitlist
+    const url = process.env.SUBSCRIBE_URL;
+    const listKey = process.env.LIST_KEY
     const param = {
     resfmt: 'JSON',
     listKey,
@@ -61,20 +68,17 @@ const addToWaitlist = async (req, res) => {
     }
     };
 
-// axios.post(url, param, config)
-//   .then(response => {
-//     return res.status(200).json( response.data );
-//   })
-//   .catch(error => {
-//     res.json(error);
-//   });
     
     try {
+
         let response = await axios.post(url, param, config)
         let data = response.data
-        return res.status(200).json( data );
+        return res.status(200).json(data);
+        
     } catch (error) {
-        return res.status(400).json( error );
+
+        return res.status(400).json(error);
+        
     }
     
 }
