d380a81b82640ae2ed978bc54b92621a271e864a
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2023-04-26 13:09:43 +0100

    added secrets

--- controllers/waitlist.js
+++ controllers/waitlist.js
@@ -1,18 +1,24 @@
-import axios from 'axios'
 import bcrypt from 'bcrypt'
 import validator from 'validator'
 
+const listKey = process.env.LIST_KEY
+const accessToken = process.env.ACCESS_TOKEN
+const refreshToken = process.env.REFRESH_TOKEN
 const addToWaitlist = async (req, res) => {
     let { email } = req.body
     let hashedEmail = req.headers['email-hash']
 
-    if (!validator.isEmail(email)) {
-        return res.status(400).send('Invalid email address');
-    }
     const match = await bcrypt.compare(email, hashedEmail);
-    console.log(match)
 
-    res.send('Success')
+    res.json({email, hashedEmail, match, listKey, accessToken, refreshToken})
+
+    // if (!validator.isEmail(email)) {
+    //     return res.status(400).send('Invalid email address');
+    // }
+    // const match = await bcrypt.compare(email, hashedEmail);
+    // console.log(match)
+
+    // res.send('Success')
 }
  
 module.exports = addToWaitlist
